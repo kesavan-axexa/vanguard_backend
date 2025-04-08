9bc084ff01ea7f6e05e8fd59253050983717701b
kesavan-axexa <kesavan.axexa@gmail.com>
2025-04-08 08:08:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+      origin: [
+      "http://localhost:5000",         // for local development
+      "https://vanguardsolar.in",      // your live frontend
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
     allowedHeaders: ["Authorization", "Content-Type"],
     exposedHeaders: ["Authorization"],
   })
